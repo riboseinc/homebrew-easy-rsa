50232a0b8fce907dc0c6a595ee668c475ebe1b5b
Sergii Ovcharenko <sergii@sovcharenko.com>
2018-06-13 17:44:06 +0100

    update to 3.0.4 added X509 dir

--- Formula/easy-rsa.rb
+++ Formula/easy-rsa.rb
@@ -1,19 +1,23 @@
 class EasyRsa < Formula
   desc "Homebrew formula for `easy-rsa`"
   homepage 'https://github.com/OpenVPN/easy-rsa'
-  url "https://github.com/OpenVPN/easy-rsa/archive/v3.0.3.tar.gz"
-  sha256 "3e41c0dfebe04332cdc07f13bf48a69e25dfc31d059e94071a9cfb9f3bba6f9a"
+  url "https://github.com/OpenVPN/easy-rsa/archive/v3.0.4.tar.gz"
+  sha256 "1d0da6cfc47b54565fe83068530ed467540879b853670257acb47d107e8f4df3"
   head "https://github.com/OpenVPN/easy-rsa.git"
 
   depends_on "openssl"
 
   devel do
-    version '3.0.3'
+    version '3.0.4'
   end
 
   def install
     inreplace "easyrsa3/vars.example",
-              "#set_var EASYRSA\t\"$PWD\"",
+              "#set_var EASYRSA\t\"${0%/*}\"",
+              "set_var EASYRSA \"#{Formula["easy-rsa"].opt_prefix}/share\""
+
+    inreplace "easyrsa3/easyrsa",
+              "set_var EASYRSA\t\t\"${0%/*}\"",
               "set_var EASYRSA \"#{Formula["easy-rsa"].opt_prefix}/share\""
 
     inreplace "easyrsa3/vars.example",
@@ -21,13 +25,14 @@ class EasyRsa < Formula
               "set_var EASYRSA_OPENSSL \"#{Formula["openssl"].opt_prefix}/bin/openssl\""
 
     inreplace "easyrsa3/easyrsa",
-              "\tlocal prog_vars=\"${0%/*}/vars\"",
-              "\tlocal prog_vars=\"#{Formula["easy-rsa"].opt_prefix}/share/vars\""
+              "set_var EASYRSA_OPENSSL\topenssl",
+              "set_var EASYRSA_OPENSSL \"#{Formula["openssl"].opt_prefix}/bin/openssl\""    
 
     share.install %w(
       easyrsa3/easyrsa
-      easyrsa3/openssl-1.0.cnf
+      easyrsa3/openssl-easyrsa.cnf
       easyrsa3/vars.example
+      easyrsa3/x509-types
     )
 
     doc.install "ChangeLog"
@@ -37,6 +42,6 @@ class EasyRsa < Formula
     doc.install "README.quickstart.md"
 
     bin.install_symlink share/"easyrsa" => "easyrsa"
-    share.install share/"vars.example" => "vars"
+    # share.install share/"vars.example" => "vars.example"
   end
 end
