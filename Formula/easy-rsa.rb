6327ebee7e6fa0fe17e3aa810cca0000cc9872b2
Chris Baker <ChrisBaker97@users.noreply.github.com>
2019-03-05 22:06:26 -0500

    Update to 3.0.6 (#17)
    * Update to 3.0.6
    
    * Remove devel block
    
    * Fix to pass `brew style`

--- Formula/easy-rsa.rb
+++ Formula/easy-rsa.rb
@@ -1,18 +1,14 @@
 class EasyRsa < Formula
   desc "Homebrew formula for `easy-rsa`"
-  homepage 'https://github.com/OpenVPN/easy-rsa'
-  url "https://github.com/OpenVPN/easy-rsa/archive/v3.0.5.tar.gz"
-  sha256 "01d0a0b0105fb80742455cef8b6336fcb67bf69c8675d3bbd66fdf741fd6d34f"
+  homepage "https://github.com/OpenVPN/easy-rsa"
+  url "https://github.com/OpenVPN/easy-rsa/archive/v3.0.6.tar.gz"
+  sha256 "2a87c0c617bbfab556c541b83fa5bfdf1c244bfb16b530ca12efb3d979e85191"
   head "https://github.com/OpenVPN/easy-rsa.git"
 
-  devel do
-    version '3.0.4'
-  end
-
-  def install    
+  def install
     inreplace "easyrsa3/vars.example",
               "#set_var EASYRSA\t\"${0%/*}\"",
-              "#set_var EASYRSA \"#{Formula["easy-rsa"].opt_prefix}/share\""    
+              "#set_var EASYRSA \"#{Formula["easy-rsa"].opt_prefix}/share\""
 
     inreplace "easyrsa3/easyrsa",
               "set_var EASYRSA\t\t\"${0%/*}\"",
@@ -22,13 +18,12 @@ class EasyRsa < Formula
               "\tprog_vars=\"${0%/*}/vars\"",
               "\tprog_vars=\"#{Formula["easy-rsa"].opt_prefix}/share/vars\""
 
-
-    share.install %w(
+    share.install %w[
       easyrsa3/easyrsa
       easyrsa3/openssl-easyrsa.cnf
       easyrsa3/vars.example
       easyrsa3/x509-types
-    )
+    ]
 
     doc.install "ChangeLog"
     doc.install "COPYING.md"
